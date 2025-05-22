ee2ff92a76b9c955a5b55f27842079fefa4e66cd
Xiker <xiker222@gmail.com>
2025-05-22 09:24:44 +0200

    xiker: main.js aldatu

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-function aldatuKolorea() {
-  const colors = ["#f5f5dc", "#ffcccc", "#ccffcc", "#ccccff", "#fff0b3"];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  document.body.style.backgroundColor = colors[randomIndex];
-}
+console.log('Kaixo XIKERetik');
